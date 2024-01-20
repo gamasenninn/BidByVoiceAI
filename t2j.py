3d8733e025c747374e1ec223bfd16e29d70ba4f1
gamasenninn <gamasenninn@yahoo.co.jp>
2024-01-20 13:11:51 +0900

    プロンプト外部化

--- t2j.py
+++ t2j.py
@@ -3,7 +3,6 @@ import os
 import sys
 import json
 import requests
-from urllib3.exceptions import InsecureRequestWarning
 from dotenv import load_dotenv
 import openai
 import json
@@ -11,56 +10,44 @@ import json
 
 # 環境変数の読み込み
 load_dotenv()
+PROMPT_FILE = os.environ["PROMPT_FILE"]
 OPEN_API_KEY = os.environ["OPEN_API_KEY"]
 openai.api_key = OPEN_API_KEY
 
-requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-
 # メッセージのテンプレート
-MESSAGE_TEMPLATE = """
-{keyword}次に示す項目ごとに効果的に変換して。
-出力は下記の項目だけを純粋な配列のJSON形式でお願いします。
-{{
-    listing=[{{
-        'listing_number':(出品番号),
-        'entry_date':(記入日),
-        'scode':(管理番号),
-        'responsible_person':(担当者),
-        'product_name':(商品名),
-        'maker':(メーカー),
-        'model':(モデル),
-        'size_depth':(サイズ縦),
-        'size_width':(サイズ横),
-        'size_height':(サイズ高さ),
-        'engine_model': (エンジン型式), 
-        'max_ps': (馬力),
-        'fuel_type':(燃料種別),
-        'plowing_width':(耕耘幅),
-        'mowing_width':(刈幅。制約1を参照),
-        'battery_size':(バッテリーサイズ),
-        'primary_transmission': (主変速。制約2),
-        'secondary_transmission':(副変速),
-        'memo':(備考またはメモ),
-        'hour':(アワーメーター),
-        'listing_details':[(出品詳細)] 
-    }}]
-}}
-#制約1
-仮幅となっている場合があります。刈幅としてください。
-#制約2
-内容がF2R1など略語になっている場合があります。次のように変換してください。
-例:
-F2R1 -> 前進1-2、後進1
-F3R3 -> 前進1-2-3、後進1-2-3
+MESSAGE_TEMPLATE = ""
+MESSAGE_PARAMS = {
+    'keyword' : "あなたは聞き分けのプロです。これから与えられた音声テキストをJSON形式で正規化します。",
+    'additional_info': "最適な変換を行えるために、最大限頑張りましょう。大丈夫、あなたは天才です。"
+}
 
-"""
+def load_template():
+    try:
+        with open(PROMPT_FILE,"r",encoding='utf-8') as f:
+            return f.read()
+
+    except FileNotFoundError:
+        print("テンプレートファイルが存在しません")
+        raise Exception("TemplateFileNotfound")
+
+def to_json(json_data,file_path):
+            # ファイル名の拡張子を.jsonに変更
+        json_file_path = file_path.split('.')[0] + '.json'
+
+        # JSONデータをファイルに保存
+        with open(json_file_path, "w", encoding='utf-8') as json_file:
+            # エスケープ文字を改行に変換して表示
+            json_data = json.loads(response)
+            json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+
+        return json_file_path
 
 def convert_to_json(src_text):
-    #req_text = extract_text(src_text)
+
+    MESSAGE_TEMPLATE = load_template()
 
     try:
-        keyword = "中古農機具店での電話のやりとりです。"
-        system_message_content = MESSAGE_TEMPLATE.format(keyword=keyword)
+        system_message_content = MESSAGE_TEMPLATE.format(**MESSAGE_PARAMS)
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-1106",
             response_format={ "type": "json_object" },
@@ -92,15 +79,8 @@ if __name__ == "__main__":
         content = f.read()
 
         response = convert_to_json(content)
-
         print(response)
-        # ファイル名の拡張子を.jsonに変更
-        json_file_path = file_path.split('.')[0] + '.json'
 
-        # JSONデータをファイルに保存
-        with open(json_file_path, "w", encoding='utf-8') as json_file:
-            # エスケープ文字を改行に変換して表示
-            json_data = json.loads(response)
-            json.dump(json_data, json_file, ensure_ascii=False, indent=4)
-
+        json_file_path = to_json(response, file_path)
         print(f'変換されたデータを {json_file_path} に保存しました。')
+
